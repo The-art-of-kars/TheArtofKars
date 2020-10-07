81e1153e44232745ceed7784e3a8fe4472d67c48
IlyasDiker <ilyasbenhssine@gmail.com>
2020-10-07 11:14:23 +0100

    add base

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet/less" type="text/css" href="style.less" />
+    <script src="//cdn.jsdelivr.net/npm/less" ></script>
+</head>
+<body>
+    <div class="headerbar">
+    <center>
+        <a href="index.php"><img src="imgs/logo.png" alt="The art of cars" width="300px"></a>
+        <div class="horizontallist">
+            <a href="karsforsale.php">Kars for sale</a>
+            <a href="">Sold Kars</a>
+            <a href="">Services</a>
+            <a href="">Contact Us</a>
+        </div>
+    </center>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The art of kars</title>
+    <link rel="icon" href="imgs/favicon.png">
+    <link rel="stylesheet/less" type="text/css" href="style.less" />
+    <script src="//cdn.jsdelivr.net/npm/less" ></script>
+    <script src="https://kit.fontawesome.com/097cd24b76.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <?php require "header.php" ?>
+    <?php require "brandsmenu.php" ?>
+    <?php require "carsearch.php" ?>
+    <div class="aboutus">
+        <h1>About us</h1>
+        <p>
+        <span>A car is an instrument just like colors are for a painter, I trully believe that driving is an art.</span><br>
+        We sell cars for you & help you find or buy the car of your deams, ease your car sale with our second hand store.<br>
+        <br>You can call us we come & take care about your car & the paper work until it's sold.<br>
+        <br>We also offer a wide range of tailor-made services to help you enjoy your KAR.</p>
+    </div>
+    <?php require "footer.php" ?>
+</body>
+</html>
